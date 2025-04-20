cb171193da6ce6d13a162c29e82efe5b0dda2a19
Alan Varghese <alanvz757@gmail.com>
2025-04-20 15:24:18 +0400

    Updated

---
+++ Provider/counter/lib/counter_screen.dart
@@ -0,0 +1,24 @@
+import 'package:counter/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<CounterProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Provider Counter")),
+      body: Center(
+        child: Text(
+          'Counter: ${counter.count}',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.increment,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ Provider/counter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:counter/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'counter_provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterScreen(),
+    );
+  }
+}
